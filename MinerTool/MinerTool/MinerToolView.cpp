9f44e1a1537c628616d9816c11d21001d8890786
billnie <706919534@qq.com>
2017-10-21 17:03:42 +0800

    dd

--- MinerTool/MinerTool/MinerToolView.cpp
+++ MinerTool/MinerTool/MinerToolView.cpp
@@ -25,7 +25,9 @@
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
-
+#include <iostream>    
+#include <boost/property_tree/ptree.hpp>    
+#include <boost/property_tree/ini_parser.hpp>    
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
