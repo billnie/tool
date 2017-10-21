17dd5f9c6adbce0f6678647881f0863e280591e5
billnie <706919534@qq.com>
2017-10-21 11:02:35 +0800

    增加boost库支持

--- MinerTool/MinerTool/MinerToolView.cpp
+++ MinerTool/MinerTool/MinerToolView.cpp
@@ -11,7 +11,10 @@
 
 #include "MinerToolDoc.h"
 #include "MinerToolView.h"
-
+#include <process.h>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
